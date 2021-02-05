ee77bbc1747994f5d5e45ffa941523fadbe988b5
Jerrett Longworth <jerrett7@gmail.com>
2021-02-04 20:26:17 -0500

    velocity.py: Format using Black linter

--- charts/velocity.py
+++ charts/velocity.py
@@ -1,60 +1,76 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class VelocityChart:
-	def __init__(self, sprints):
-		self.sprints = sprints
+    def __init__(self, sprints):
+        self.sprints = sprints
 
-	def makeGraph(self):
-		label_locations = np.arange(len(self.sprints))
-		bar_width = 0.35
+    def makeGraph(self):
+        label_locations = np.arange(len(self.sprints))
+        bar_width = 0.35
 
-		commitment_values = []
-		completed_values = []
-		for sprint in self.sprints:
-			commitment_values.append(sprint.commitment)
-			completed_values.append(sprint.completed)
+        commitment_values = []
+        completed_values = []
+        for sprint in self.sprints:
+            commitment_values.append(sprint.commitment)
+            completed_values.append(sprint.completed)
 
-		fig, ax = plt.subplots()
-		commitment_bar = ax.bar(label_locations - bar_width / 2, commitment_values, bar_width, label = 'Comitment')
-		completed_bar = ax.bar(label_locations + bar_width / 2, completed_values, bar_width, label = 'Completed')
+        fig, ax = plt.subplots()
+        commitment_bar = ax.bar(
+            label_locations - bar_width / 2,
+            commitment_values,
+            bar_width,
+            label="Comitment",
+        )
+        completed_bar = ax.bar(
+            label_locations + bar_width / 2,
+            completed_values,
+            bar_width,
+            label="Completed",
+        )
 
+        """ Credit to: https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/barchart.html"""
 
-		""" Credit to: https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/barchart.html"""
-		def autolabel(rects):
-		    """Attach a text label above each bar in *rects*, displaying its height."""
-		    for rect in rects:
-		        height = rect.get_height()
-		        ax.annotate('{}'.format(height),
+        def autolabel(rects):
+            """Attach a text label above each bar in *rects*, displaying its height."""
+            for rect in rects:
+                height = rect.get_height()
+                ax.annotate(
+                    "{}".format(height),
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
-               	 	ha='center',
-               	 	va='bottom')
+                    ha="center",
+                    va="bottom",
+                )
 
-		autolabel(commitment_bar)
-		autolabel(completed_bar)
+        autolabel(commitment_bar)
+        autolabel(completed_bar)
 
-		fig.tight_layout()
+        fig.tight_layout()
 
-		ax.set_ylabel('Scores')
-		ax.set_title('Velocity Chart')
-		ax.set_xticks(label_locations)
-		ax.set_xticklabels([sprint.name for sprint in self.sprints])
-		ax.legend()
+        ax.set_ylabel("Scores")
+        ax.set_title("Velocity Chart")
+        ax.set_xticks(label_locations)
+        ax.set_xticklabels([sprint.name for sprint in self.sprints])
+        ax.legend()
 
-		plt.savefig('velocity_chart.png', bbox_inches='tight')
+        plt.savefig("velocity_chart.png", bbox_inches="tight")
+
 
 class Sprint:
-	def __init__(self, name, commitment, completed):
-		self.name = name
-		self.commitment = commitment
-		self.completed = completed
+    def __init__(self, name, commitment, completed):
+        self.name = name
+        self.commitment = commitment
+        self.completed = completed
+
 
 def main():
-	sprint1 = Sprint("Sprint1", 2, 1)
-	velocity_chart = VelocityChart([sprint1])
-	velocity_chart.makeGraph()
+    sprint1 = Sprint("Sprint1", 2, 1)
+    velocity_chart = VelocityChart([sprint1])
+    velocity_chart.makeGraph()
+
 
 if __name__ == "__main__":
-	main()
+    main()
