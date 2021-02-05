4441f257c260619a2f80211fdad391b5f6a15e81
Jerrett Longworth <jerrett7@gmail.com>
2021-02-05 10:39:53 -0500

    charts: Reformat using Black

--- charts/burndown.py
+++ charts/burndown.py
@@ -3,7 +3,7 @@ import numpy as np
 from sprint import Sprint
 
 
-class  BurndownChart:
+class BurndownChart:
     def __init__(self, total_points):
         self.sprints = []
         self.total_points = total_points
@@ -12,23 +12,23 @@ class  BurndownChart:
         self.sprints.append(Sprint(name, commitment, completed))
 
     def makeGraph(self):
-		names = [sprint.name for sprint in self.sprints]
+        names = [sprint.name for sprint in self.sprints]
 
-		fig, ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-		temp_points = self.total_points
-		points_left = []
-		for sprint in self.sprints:
-			points_left.append(temp_points)
-			temp_points -= sprint.completed
+        temp_points = self.total_points
+        points_left = []
+        for sprint in self.sprints:
+            points_left.append(temp_points)
+            temp_points -= sprint.completed
 
-		x = [i for i in range(len(names))]
-		plt.plot(x, points_left, color='red', marker='o')
-		plt.title('Burndown Chart')
-		plt.xlabel('Sprints')
-		ax.set_xticks(x)
-		plt.ylabel('Estimated points remaining')
-		ax.set_yticks(np.arange(10, self.total_points + 15, 10))
-		plt.grid(True)
-		plt.savefig("exported/burndown_chart.png", bbox_inches="tight")
-		plt.close()
+        x = [i for i in range(len(names))]
+        plt.plot(x, points_left, color="red", marker="o")
+        plt.title("Burndown Chart")
+        plt.xlabel("Sprints")
+        ax.set_xticks(x)
+        plt.ylabel("Estimated points remaining")
+        ax.set_yticks(np.arange(10, self.total_points + 15, 10))
+        plt.grid(True)
+        plt.savefig("exported/burndown_chart.png", bbox_inches="tight")
+        plt.close()
--- charts/main.py
+++ charts/main.py
@@ -1,28 +1,31 @@
 from velocity import VelocityChart
 from burndown import BurndownChart
 
+
 class Charts:
     def __init__(self, total_points):
-    	self.velocity_chart = VelocityChart()
-    	self.burndown_chart = BurndownChart(total_points)
+        self.velocity_chart = VelocityChart()
+        self.burndown_chart = BurndownChart(total_points)
 
     def addSprint(self, name, commitment, completed):
         self.velocity_chart.addSprint(name, commitment, completed)
         self.burndown_chart.addSprint(name, commitment, completed)
 
     def makeGraphs(self):
-    	self.velocity_chart.makeGraph()
-    	self.burndown_chart.makeGraph()
+        self.velocity_chart.makeGraph()
+        self.burndown_chart.makeGraph()
+
 
 def main():
-	total_points = 100
-	charts = Charts(total_points)
+    total_points = 100
+    charts = Charts(total_points)
 
-	charts.addSprint("Sprint1", 2, 1)
-	charts.addSprint("Sprint2", 5, 5)
-	charts.addSprint("Sprint3", 10, 7)
-	charts.addSprint("Sprint4", 3, 3)
-	charts.makeGraphs()
+    charts.addSprint("Sprint1", 2, 1)
+    charts.addSprint("Sprint2", 5, 5)
+    charts.addSprint("Sprint3", 10, 7)
+    charts.addSprint("Sprint4", 3, 3)
+    charts.makeGraphs()
+
 
 if __name__ == "__main__":
     main()
