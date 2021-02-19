0363b8cd847dd2236f29f6bd3073c2d7d56a10a4
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-02-19 15:37:10 -0500

    Simplify AccountController as it may have caused too many redirects

--- project/src/Clippy/Controllers/AccountController.cs
+++ project/src/Clippy/Controllers/AccountController.cs
@@ -5,34 +5,18 @@ using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Clippy.Controllers {
-    [Authorize]
-    public class AccountController : Controller {
-        private readonly ILogger _logger;
-        private int LogoutEventId = 42;
-
-        public AccountController(ILogger<AccountController> logger)
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Signin(string returnUrl = "/")
         {
-            _logger = logger;
-        }
-
-        [AllowAnonymous]
-        public async Task<IActionResult> Login(string returnUrl = null)
-        {
-            if (HttpContext.User != null && HttpContext.User.Identity.IsAuthenticated) {
-                returnUrl = null;
-            }
-
-            // Clear existing cookie to ensure clean login
-            await HttpContext.SignOutAsync();
-
             return Challenge(new AuthenticationProperties()  { RedirectUri = returnUrl });
         }
 
-        [HttpPost]
-        public async Task<IActionResult> LogOff()
+        [HttpGet]
+        public async Task<IActionResult> Signout()
         {
             await HttpContext.SignOutAsync();
-            _logger.LogInformation(LogoutEventId, "User logged out.");
             return Redirect("~/");
         }
     }
