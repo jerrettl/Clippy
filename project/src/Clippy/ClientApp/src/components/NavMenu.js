6da5ddcd05a0f9c5a8e56db95149e67a9f16583a
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-02-19 01:12:51 -0500

    Add Home Page UI
    Co-Authored-By: Diego Rodrigues <di.rodrigues003@gmail.com>

--- project/src/Clippy/ClientApp/src/components/NavMenu.js
+++ project/src/Clippy/ClientApp/src/components/NavMenu.js
@@ -24,9 +24,11 @@ export class NavMenu extends Component {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 background-color-primary " light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Clippy</NavbarBrand>
+            <NavbarBrand  tag={Link} to="/">
+              <img src={require('../assets/logo.png')} width="156px" alt="logo" />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
