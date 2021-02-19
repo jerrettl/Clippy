de9ba7bb23eeec6d43db660dad3b8352c9a31987
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-02-19 01:03:18 -0500

    Revert "Feb. 02/19/21"
    This reverts commit d9c536faff2df3139592172eaea7fa0ada9eb521.

--- project/src/Clippy/ClientApp/src/components/Layout.js
+++ project/src/Clippy/ClientApp/src/components/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
@@ -8,7 +9,9 @@ export class Layout extends Component {
     return (
       <div>
         <NavMenu />
+        <Container>
           {this.props.children}
+        </Container>
       </div>
     );
   }
--- project/src/Clippy/ClientApp/src/components/NavMenu.js
+++ project/src/Clippy/ClientApp/src/components/NavMenu.js
@@ -24,11 +24,9 @@ export class NavMenu extends Component {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 background-color-primary " light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand  tag={Link} to="/">
-              <img src={require('../assets/logo.png')} width="156px" alt="logo" />
-            </NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Clippy</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
