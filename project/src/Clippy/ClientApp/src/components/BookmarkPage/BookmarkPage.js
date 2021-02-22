ed4ca5e3033498f9aaaeee83131de5ae7ed4ede4
Jaeivan <jaeivanromero@gmail.com>
2021-02-21 20:00:37 -0500

    Restructure components folder

--- project/src/Clippy/ClientApp/src/components/BookmarkPage/BookmarkPage.js
+++ project/src/Clippy/ClientApp/src/components/BookmarkPage/BookmarkPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import UserNavbar from './UserNavbar'
+import UserNavbar from './../UserNavbar/UserNavbar'
 
 export default class BookmarkPage extends Component {
     render() {
