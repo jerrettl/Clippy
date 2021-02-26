df62655c4f826e57fb8586ff1c26de34be970e0b
Jaeivan <jaeivanromero@gmail.com>
2021-02-25 22:19:45 -0500

    Add basic about page content

--- project/src/Clippy/ClientApp/src/components/AboutContent/AboutContent.js
+++ project/src/Clippy/ClientApp/src/components/AboutContent/AboutContent.js
@@ -3,6 +3,21 @@ import React from 'react'
 export default function AboutContent() {
     return (
         <div>
+          <h1> Product Vision </h1>
+          <p>
+            For internet users who find themselves having a difficult time organizing and discovering resources on the Web that sparks their interest, we introduce Clippy. Clippy is a social bookmarking service designed for users to explore and manage resources on the Internet. Users can immerse themselves in their own distraction-free space curated by them or explore resources gathered by others in the community. Unlike many other social bookmarking services, such as Pocket, Clippy is currently open source for anyone to adapt or use for themselves.
+          </p>
+
+          <h1> Team Members </h1>
+          <p>
+            Team Member <a href="https://github.com"> Github </a>
+          </p>
+
+          <h1> Source Code </h1>
+          <p>
+            Clippy is open source! Feel free to check out the source code at our github.
+            <a href="https://github.com/Clippy5/Clippy">Source Code</a>
+          </p>
         </div>
     )
 }
