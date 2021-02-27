81b574d14393641223945ddc4517c9cf0c1c566e
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-02-27 11:47:50 -0500

    Add latest resources to Clippy API homepage

--- project/src/Clippy/Pages/Index.cshtml.cs
+++ project/src/Clippy/Pages/Index.cshtml.cs
@@ -1,9 +1,23 @@
+using Clippy.Data;
+using Clippy.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Clippy.Pages {
-    public class Index : PageModel {
-        public void OnGet() {
+    public class IndexModel : PageModel {
+        private ClippyContext _context;
 
+        public IndexModel(ClippyContext context) {
+            _context = context;
+        }
+
+         public IList<Resource> Resources { get; set; }
+
+        public async Task OnGetAsync() {
+            Resources = await _context.GetResourcesAsync();
+            Resources = Resources.Take(6).ToList();
         }
     }
 }
