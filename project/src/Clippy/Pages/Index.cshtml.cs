c6e5e035290d1cb4c52ee4797e00ee6b9697bb9a
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-02-27 11:43:38 -0500

    Move page title

--- project/src/Clippy/Pages/Index.cshtml.cs
+++ project/src/Clippy/Pages/Index.cshtml.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Clippy.Pages {
     public class Index : PageModel {
-        [ViewData]
-        public string Title { get; } = "Home";
-
         public void OnGet() {
 
         }
