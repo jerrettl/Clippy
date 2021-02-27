b8345658ee3357823d42727c788803bc6cdedb51
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-02-27 17:18:03 -0500

    Add Admin page to edit resources

---
+++ project/src/Clippy/Pages/Admin/Resources/Edit.cshtml.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading.Tasks;
+using Clippy.Data;
+using Clippy.Entities;
+using Clippy.Helpers;
+using Clippy.Models.Admin;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clippy.Pages.Admin.Resources
+{
+    public class EditModel : PageModel
+    {
+        private ClippyContext _context;
+
+        public EditModel(ClippyContext context) {
+            _context = context;
+        }
+
+        [BindProperty]
+        public EditResourceModel Resource { get; set; }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            var resource = await _context.GetResourceAsync(id);
+            if (resource == null)
+                return RedirectToPage("./Index");
+
+            Resource = new EditResourceModel
+            {
+                Title = resource.Metadata["Title"],
+                Location = resource.Location,
+                MediaType = resource.Metadata.ContainsKey("MediaType") ? resource.Metadata["MediaType"] : "",
+                Image = resource.Metadata.ContainsKey("Image") ? resource.Metadata["Image"] : "",
+                Description = resource.Metadata.ContainsKey("Description") ? resource.Metadata["Description"] : ""
+            };
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var existingResource = await _context.GetResourceAsync(id);
+            if (existingResource == null)
+                return RedirectToPage("./Index");
+
+            if (Resource.Location != existingResource.Location) {
+                var existingLocation = await _context.GetResourceByLocationAsync(Resource.Location);
+                if (existingLocation != null)
+                    ModelState.AddModelError("Location", $"Resource already exists: Id = {existingLocation.Id}.");
+            }
+
+            if (!ModelState.IsValid)
+                return Page();
+
+            existingResource.Location = Resource.Location;
+            existingResource.Metadata.MergeMetadata(Resource.FetchMetadata());
+
+            _context.Update(existingResource);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
