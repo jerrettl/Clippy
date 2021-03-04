201320af02f6478f408480f2af177ebb80667573
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-04 12:43:52 -0500

    Add User entity

---
+++ project/src/Clippy/Entities/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clippy.Entities
+{
+    public class User
+    {
+        public int Id  { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+    }
+}
