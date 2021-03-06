cd81e088d5b0fc14a035482cf5baf8ddd298001e
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-06 09:31:02 -0500

    Add test to verify OnGet retrieves correct user

---
+++ project/test/Clippy.Tests/Admin/Users/DetailsPageTests.cs
@@ -0,0 +1,37 @@
+using Clippy.Data;
+using Clippy.Data.Helpers;
+using Clippy.Entities;
+using Clippy.Pages.Admin.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Clippy.Tests.Admin.Users
+{
+    public class DetailsPageTests
+    {
+        [Theory]
+        [InlineData(1)]
+        public async Task OnGetAsync_PopulatesThePageModel_WithAUser(int userId)
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            var expectedUser = DatabaseInitializer.GetSeedingUsers().Single(u => u.Id == userId);
+            mockContext.Setup(
+                db => db.GetUserAsync(userId)).Returns(Task.FromResult(expectedUser));
+            var pageModel = new DetailsModel(mockContext.Object);
+
+            // Act
+            await pageModel.OnGetAsync(userId);
+
+            // Assert
+            var actualUser = Assert.IsAssignableFrom<User>(pageModel.UserEntity);
+            Assert.Equal(expectedUser.Username, actualUser.Username);
+        }
+    }
+}
