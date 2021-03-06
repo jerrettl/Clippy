7464e98174a2d169ed34b7811a404be6fc411039
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-06 11:12:32 -0500

    Add test to verify unique username requirement

--- project/test/Clippy.Tests/Admin/Users/EditPageTests.cs
+++ project/test/Clippy.Tests/Admin/Users/EditPageTests.cs
@@ -1,5 +1,6 @@
 using Clippy.Data;
 using Clippy.Data.Helpers;
+using Clippy.Entities;
 using Clippy.Models.Admin;
 using Clippy.Pages.Admin.Users;
 using Microsoft.AspNetCore.Http;
@@ -70,6 +71,43 @@ namespace Clippy.Tests.Admin.Users
 
             // Act
             var result = await pageModel.OnPostAsync(userId);
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+        }
+
+        [Theory]
+        [InlineData(1, "Clippy6")]
+        public async Task OnPostAsync_ReturnsAPageResult_WhenChangedUsernameExists(int existingUserId, string otherUsername)
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            var existingUser = DatabaseInitializer.GetSeedingUsers().Single(u => u.Id == existingUserId);
+            var otherUser = new User { Username = otherUsername, Name = existingUser.Username};
+            mockContext.Setup(db => db.GetUserAsync(existingUser.Id)).Returns(Task.FromResult(existingUser));
+            mockContext.Setup(db => db.GetUserByUsernameAsync(otherUsername)).Returns(Task.FromResult(otherUser));
+            var httpContext = new DefaultHttpContext();
+            var modelState = new ModelStateDictionary();
+            var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            var pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData
+            };
+            var pageModel = new EditModel(mockContext.Object)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+                Url = new UrlHelper(actionContext),
+                UserEntity = new EditUserModel {Username = otherUsername, Name = existingUser.Name}
+            };
+
+            // Act
+            var result = await pageModel.OnPostAsync(existingUserId);
 
             // Assert
             Assert.IsType<PageResult>(result);
