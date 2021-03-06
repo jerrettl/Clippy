ad29fd812b737f5766c4dc436e92015a081a2fb2
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-06 09:38:00 -0500

    Add test to verify redirection when user doesn't exist

--- project/test/Clippy.Tests/Admin/Users/DetailsPageTests.cs
+++ project/test/Clippy.Tests/Admin/Users/DetailsPageTests.cs
@@ -53,5 +53,27 @@ namespace Clippy.Tests.Admin.Users
             // Assert
             Assert.IsType<RedirectToPageResult>(result);
         }
+
+        [Theory]
+        [InlineData(100000000)]
+        [InlineData(200000000)]
+        [InlineData(300000000)]
+        [InlineData(400000000)]
+        public async Task OnPostDeleteAsync_ReturnsARedirectToPageWhenUserDoesntExist(int userId)
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            mockContext.Setup(
+                db => db.GetUserAsync(userId)).Returns(Task.FromResult((User)null));
+            var pageModel = new DetailsModel(mockContext.Object);
+
+            // Act
+            var result = await pageModel.OnPostDeleteAsync(userId);
+
+            // Assert
+            Assert.IsType<RedirectToPageResult>(result);
+        }
     }
 }
