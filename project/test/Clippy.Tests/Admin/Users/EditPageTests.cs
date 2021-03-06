c831a9d8727ee75e358801f690d6b90baf7269c1
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-06 11:00:12 -0500

    Add test to verify page re-displays on input errors

--- project/test/Clippy.Tests/Admin/Users/EditPageTests.cs
+++ project/test/Clippy.Tests/Admin/Users/EditPageTests.cs
@@ -39,5 +39,40 @@ namespace Clippy.Tests.Admin.Users
             var actualUser = Assert.IsAssignableFrom<EditUserModel>(pageModel.UserEntity);
             Assert.Equal(expectedUser.Username, actualUser.Username);
         }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task OnPostAsync_ReturnsAPageResult_WhenModelStateIsInvalid(int userId)
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            var expectedUser = DatabaseInitializer.GetSeedingUsers().Single(u => u.Id == userId);
+            mockContext.Setup(db => db.GetUserAsync(userId)).Returns(Task.FromResult(expectedUser));
+            var httpContext = new DefaultHttpContext();
+            var modelState = new ModelStateDictionary();
+            var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            var pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData
+            };
+            var pageModel = new EditModel(mockContext.Object)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+                Url = new UrlHelper(actionContext)
+            };
+            pageModel.ModelState.AddModelError("Username", "Username is required.");
+
+            // Act
+            var result = await pageModel.OnPostAsync(userId);
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+        }
     }
 }
