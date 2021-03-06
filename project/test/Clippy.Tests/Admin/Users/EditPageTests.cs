653878234bd24d042ade7f31bab22b2fe16be05a
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-06 10:55:33 -0500

    Add test to verify OnGet retrieves correct user

---
+++ project/test/Clippy.Tests/Admin/Users/EditPageTests.cs
@@ -0,0 +1,43 @@
+using Clippy.Data;
+using Clippy.Data.Helpers;
+using Clippy.Models.Admin;
+using Clippy.Pages.Admin.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Clippy.Tests.Admin.Users
+{
+    public class EditPageTests
+    {
+        [Theory]
+        [InlineData(1)]
+        public async Task OnGetAsync_PopulatesThePageModel_WithAUser(int userId)
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            var expectedUser = DatabaseInitializer.GetSeedingUsers().Single(u => u.Id == userId);
+            mockContext.Setup(
+                db => db.GetUserAsync(userId)).Returns(Task.FromResult(expectedUser));
+            var pageModel = new EditModel(mockContext.Object);
+
+            // Act
+            await pageModel.OnGetAsync(userId);
+
+            // Assert
+            var actualUser = Assert.IsAssignableFrom<EditUserModel>(pageModel.UserEntity);
+            Assert.Equal(expectedUser.Username, actualUser.Username);
+        }
+    }
+}
