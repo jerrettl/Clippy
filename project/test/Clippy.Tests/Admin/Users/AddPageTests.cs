ee4f381ee7c151334cef5bd5afef0cfaa2cd48f7
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-06 09:42:22 -0500

    Add test to verify page re-displays on input errors

---
+++ project/test/Clippy.Tests/Admin/Users/AddPageTests.cs
@@ -0,0 +1,56 @@
+using Clippy.Data;
+using Clippy.Data.Helpers;
+using Clippy.Models.Admin;
+using Clippy.Pages.Admin.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Clippy.Tests.Admin.Users
+{
+    public class AddPageTests
+    {
+        [Fact]
+        public async Task OnPostAsync_ReturnsAPageResult_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            var expectedUsers = DatabaseInitializer.GetSeedingUsers();
+            mockContext.Setup(db => db.GetUsersAsync()).Returns(Task.FromResult(expectedUsers));
+            var httpContext = new DefaultHttpContext();
+            var modelState = new ModelStateDictionary();
+            var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            var pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData
+            };
+            var pageModel = new AddModel(mockContext.Object)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+                Url = new UrlHelper(actionContext)
+            };
+            pageModel.ModelState.AddModelError("Username", "Username is required.");
+
+            // Act
+            var result = await pageModel.OnPostAsync();
+
+            // Assert
+            Assert.IsType<PageResult>(result);
+        }
+    }
+}
