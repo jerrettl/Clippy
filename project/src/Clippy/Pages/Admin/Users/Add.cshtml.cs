a28f3da8c683f5529e590c190672e2542bae6f4c
Jerrett Longworth <jerrett7@gmail.com>
2021-03-05 20:42:58 -0500

    Add functionality for adding users
    Co-authored-by: Jordyn Hayden <jordynhayden@users.noreply.github.com>
    Co-authored-by: Joshua Frazer <joshiefrazie@Knights.ucf.edu>
    Co-authored-by: Diego Rodrigues <di.rodrigues003@gmail.com>

---
+++ project/src/Clippy/Pages/Admin/Users/Add.cshtml.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading.Tasks;
+using Clippy.Data;
+using Clippy.Entities;
+using Clippy.Models.Admin;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clippy.Pages.Admin.Users
+{
+    public class AddModel : PageModel
+    {
+        private ClippyContext _context;
+
+        public AddModel(ClippyContext context) {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AddUserModel UserEntity { get; set; }
+
+        public void OnGet() {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var existingUser = await _context.GetUserByUsernameAsync(UserEntity.Username);
+            if (existingUser != null) {
+                ModelState.AddModelError("Username", $"Username is already taken: {existingUser.Username}.");
+            }
+
+            if (!ModelState.IsValid)
+                return Page();
+
+            var user = new User
+            {
+                Username = UserEntity.Username,
+                Name = UserEntity.Name,
+                CreateDate = DateTime.UtcNow
+            };
+
+            _context.AddUser(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
