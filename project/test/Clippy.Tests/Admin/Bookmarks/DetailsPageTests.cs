a98e8543457f5481cd50fa92aa711f90680d22ab
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-07 13:25:19 -0500

    Add test to verify deleting bookmark redirects to index page

--- project/test/Clippy.Tests/Admin/Bookmarks/DetailsPageTests.cs
+++ project/test/Clippy.Tests/Admin/Bookmarks/DetailsPageTests.cs
@@ -37,5 +37,29 @@ namespace Clippy.Tests.Admin.Bookmarks
             var actualBookmark = Assert.IsAssignableFrom<Bookmark>(pageModel.Bookmark);
             Assert.Equal(expectedBookmark.Id, actualBookmark.Id);
         }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public async Task OnPostDeleteAsync_ReturnsARedirectToPageResult(int bookmarkId)
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ClippyContext>()
+                .UseInMemoryDatabase("InMemoryDb");
+            var mockContext = new Mock<ClippyContext>(optionsBuilder.Options);
+            var expectedBookmark = DatabaseInitializer.GetSeedingBookmarks().Single(b => b.Id == bookmarkId);
+            mockContext.Setup(
+                db => db.GetBookmarkAsync(bookmarkId)).Returns(Task.FromResult(expectedBookmark));
+            var pageModel = new DetailsModel(mockContext.Object);
+
+            // Act
+            var result = await pageModel.OnPostDeleteAsync(bookmarkId);
+
+            // Assert
+            Assert.IsType<RedirectToPageResult>(result);
+        }
     }
 }
