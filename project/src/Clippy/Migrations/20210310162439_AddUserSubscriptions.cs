bf7898c15fdfe4936231d9192dbba6d985aa858b
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-10 13:23:03 -0500

    Add AddUserSubscriptions data migrations

---
+++ project/src/Clippy/Migrations/20210310162439_AddUserSubscriptions.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clippy.Migrations
+{
+    public partial class AddUserSubscriptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserUser",
+                columns: table => new
+                {
+                    FollowersId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubscriptionsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUser", x => new { x.FollowersId, x.SubscriptionsId });
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_FollowersId",
+                        column: x => x.FollowersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_SubscriptionsId",
+                        column: x => x.SubscriptionsId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 105, DateTimeKind.Utc).AddTicks(5970));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 105, DateTimeKind.Utc).AddTicks(5970));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 105, DateTimeKind.Utc).AddTicks(5970));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 105, DateTimeKind.Utc).AddTicks(5970));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 105, DateTimeKind.Utc).AddTicks(5970));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 102, DateTimeKind.Utc).AddTicks(9970));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 102, DateTimeKind.Utc).AddTicks(9970));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 102, DateTimeKind.Utc).AddTicks(9970));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 102, DateTimeKind.Utc).AddTicks(9970));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 102, DateTimeKind.Utc).AddTicks(9970));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 102, DateTimeKind.Utc).AddTicks(9970));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 10, 16, 24, 39, 104, DateTimeKind.Utc).AddTicks(6130));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUser_SubscriptionsId",
+                table: "UserUser",
+                column: "SubscriptionsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserUser");
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 918, DateTimeKind.Utc).AddTicks(2850));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 918, DateTimeKind.Utc).AddTicks(2850));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 918, DateTimeKind.Utc).AddTicks(2850));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 918, DateTimeKind.Utc).AddTicks(2850));
+
+            migrationBuilder.UpdateData(
+                table: "Bookmarks",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 918, DateTimeKind.Utc).AddTicks(2850));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 916, DateTimeKind.Utc).AddTicks(670));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 916, DateTimeKind.Utc).AddTicks(670));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 916, DateTimeKind.Utc).AddTicks(670));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 916, DateTimeKind.Utc).AddTicks(670));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 916, DateTimeKind.Utc).AddTicks(670));
+
+            migrationBuilder.UpdateData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 916, DateTimeKind.Utc).AddTicks(670));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2021, 3, 9, 16, 25, 14, 917, DateTimeKind.Utc).AddTicks(6040));
+        }
+    }
+}
