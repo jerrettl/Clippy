4299cea7735b2008b4be781c04b53849924b4ae6
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-03-13 00:03:46 -0500

    Adding drop down for Avatar

--- project/src/Clippy/wwwroot/js/main.js
+++ project/src/Clippy/wwwroot/js/main.js
@@ -1,4 +1,10 @@
 let navLinks = document.querySelector(".nav-links");
+var dd_parent = document.querySelector(".dd-parent");
+var dd_menu = document.querySelector(".dd-menu");
+
+dd_parent.addEventListener("click", function(){
+    dd_menu.classList.toggle("active");
+})
 
 function handleClick() {
 
