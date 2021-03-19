cf552deabf6c136717fbc5f26b1299802e71d577
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-19 19:40:50 -0400

    Remove API controllers

--- project/src/Clippy/Controllers/Api/ApiController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Clippy.Controllers.Api {
-    [ApiController]
-    [Route("api/[controller]")]
-    [Produces("application/json")]
-    [Consumes("application/json")]
-    public class ApiController : ControllerBase {}
-}
