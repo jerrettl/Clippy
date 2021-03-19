8507808420f77db24f43f112728c859099d24830
Jerrett Longworth <jerrett7@gmail.com>
2021-03-19 03:47:43 -0400

    ImageDownloaderHelper: Use fallback methods

--- project/src/Clippy/Helpers/ImageDownloaderHelper.cs
+++ project/src/Clippy/Helpers/ImageDownloaderHelper.cs
@@ -11,12 +11,30 @@ namespace Clippy.Helpers
     {
         const int MAX_TRIES = 5;
 
-        // Take the URL of a website as input, and download the first available
-        // image from that page. Returns the name of the file that was downloaded.
+        // Take the URL of a website as input, and attempt to download an image
+        // from it using different methods. Returns the name of the file that was downloaded.
+        // If every method fails, return null.
         public static async Task<string> FetchImageFromUrl(string url)
         {
             if (url == null) return null;
 
+            Tuple<string, string> pageData = await FetchPage(url);
+            if (pageData == null) return null;
+            string resolvedUrl = pageData.Item1;
+            string pageContent = pageData.Item2;
+            if (resolvedUrl == null) return null;
+
+            string image = FetchImageFromUrlUsingPage(resolvedUrl, pageContent);
+            if (image == null) image = FetchImageFromUrlUsingFavicon(resolvedUrl, pageContent);
+
+            return image;
+        }
+
+        // Take the URL of a website as input and attempt to resolve it.
+        // Returns a tuple containing the final resolved URL of the page and
+        // the HTML contents of the retrieved page.
+        private static async Task<Tuple<string, string>> FetchPage(string url)
+        {
             // Add a protocol to the URL in the case that it doesn't already have one.
             if (!url.StartsWith("http://") && !url.StartsWith("https://"))
             {
@@ -26,6 +44,7 @@ namespace Clippy.Helpers
             // Setup for making requests to the internet.
             HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false };
             HttpClient client = new HttpClient(handler);
+            client.Timeout = TimeSpan.FromSeconds(5);
             HttpRequestMessage request = null;
             HttpResponseMessage response = null;
             string pageContent = null;
@@ -62,6 +81,12 @@ namespace Clippy.Helpers
                     Console.Error.WriteLine(exception);
                     return null;
                 }
+                catch (TaskCanceledException exception)
+                {
+                    // The request probably timed out.
+                    Console.Error.WriteLine(exception);
+                    return null;
+                }
 
                 // Save the page contents.
                 pageContent = await response.Content.ReadAsStringAsync();
@@ -80,6 +105,82 @@ namespace Clippy.Helpers
                 triesMade++;
             }
 
+            return new Tuple<string, string>(url, pageContent);
+        }
+
+        // Download and store an image with the provided URL. Returns the name
+        // of the image file downloaded.
+        private static string DownloadImage(string url)
+        {
+            return DownloadImage(url, url);
+        }
+
+        // Download an image using a short URL and a long URL. The short URL is
+        // a truncated version of the long URL if it is too long for a file name.
+        // Returns the name of the image file downloaded.        
+        private static string DownloadImage(string shortUrl, string longUrl)
+        {
+            string newFileName = Guid.NewGuid().ToString() + "_" + System.IO.Path.GetFileName(shortUrl);
+            string localImagePath = "wwwroot/images/bookmarks/" + newFileName;
+
+            // Download the image.
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(longUrl, localImagePath);
+            }
+            catch (WebException exception)
+            {
+                // Possibly a 404 error. Final catch-all if the website is finicky.
+                Console.Error.WriteLine($"Unable to get image from {longUrl}");
+                Console.Error.WriteLine(exception);
+                return null;
+            }
+
+            return newFileName;
+        }
+
+        // Given a page URL and an image URL, combine them to generate a direct link
+        // to the image. The page URL is a base that may or may not be prepended to
+        // the image URL. The image URL may or may not be relative to the page it is on.
+        // Expects pageUrl to end with "/".
+        // Example:
+        //     Input: "https://www.google.com/", "/static/assets/image.jpg"
+        //     Output: "https://www.google.com/static/assets/image.jpg"
+        // Example:
+        //     Input: "https://www.example.com/", "//amazon.com/images/image.png"
+        //     Output: "http://amazon.com/images/image.png"
+        private static string GenerateImageUrl(string pageUrl, string imageUrl)
+        {
+            string result;
+
+            if (imageUrl.StartsWith("http://") ||
+                imageUrl.StartsWith("https://"))
+            {
+                result = imageUrl;
+            }
+            else if (imageUrl.StartsWith("//"))
+            {
+                result = $"http:{imageUrl}";
+            }
+            else if (imageUrl.StartsWith("/"))
+            {
+                result = $"{pageUrl}{imageUrl.Split("/", 2)[1]}";
+            }
+            else
+            {
+                result = $"{pageUrl}{imageUrl}";
+            }
+
+            return result;
+        }
+
+        // Takes as input a (resolved) page URL and the HTML contents of a page, and
+        // downloads the first image in the page. Returns the name of the saved image.
+        private static string FetchImageFromUrlUsingPage(string url, string pageContent)
+        {
+            if (pageContent == null) return null;
+
             // Parse the retrieved HTML.
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(pageContent);
@@ -89,42 +190,58 @@ namespace Clippy.Helpers
             if (firstImage == null) return null;
 
             // Generate the full URL to the image.
-            string imageSrc = firstImage.Attributes["src"].Value;
-            string imageUrl;
-            if (imageSrc.StartsWith("http://") ||
-                imageSrc.StartsWith("https://"))
-            {
-                imageUrl = imageSrc;
-            }
-            else if (imageSrc.StartsWith("//"))
-            {
-                imageUrl = $"http:{imageSrc}";
-            }
-            else if (imageSrc.StartsWith("/"))
-            {
-                imageUrl = $"{url}{imageSrc.Split("/", 2)[1]}";
-            }
-            else
-            {
-                imageUrl = $"{url}{imageSrc}";
-            }
+            string imageUrl = GenerateImageUrl(url, firstImage.Attributes["src"].Value);
 
             // Download the image using a unique identifier.
             string strippedImageUrl = (imageUrl.Contains("?")) ? imageUrl.Split("?")[0] : imageUrl;
-            string newFileName = Guid.NewGuid().ToString() + "_" + System.IO.Path.GetFileName(strippedImageUrl);
-            string localImagePath = "wwwroot/images/bookmarks/" + newFileName;
+            string newFileName = DownloadImage(strippedImageUrl, imageUrl);
 
-            // Download the image.
-            try
+            return newFileName;
+        }
+
+        // Takes as input a (resolved) page URL and downloads a favicon from that page.
+        // Returns the name of the saved image.
+        private static string FetchImageFromUrlUsingFavicon(string url, string pageContent)
+        {
+            Uri uri = new Uri(url);
+
+            // First using the simplest approach: Find favicon.ico relative to the domain
+            // name of the URL.
+            string domain = uri.GetLeftPart(UriPartial.Authority);
+            string faviconUrl = domain + "/favicon.ico";
+
+            string newFileName = DownloadImage(faviconUrl);
+
+            // If that doesn't work out, find the HTML tag that specifies an icon.
+            if (newFileName == null)
             {
-                WebClient webClient = new WebClient();
-                webClient.DownloadFile(imageUrl, localImagePath);
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(pageContent);
+
+                HtmlNode iconElement = doc.DocumentNode.SelectSingleNode("//link[@rel=\"shortcut icon\"]");
+                if (iconElement != null)
+                {
+                    faviconUrl = GenerateImageUrl(domain + "/", iconElement.Attributes["href"].Value);
+                    newFileName = DownloadImage(faviconUrl);
+                }
             }
-            catch (WebException exception)
+            
+            // Try the previous method, but using a slightly different tag.
+            if (newFileName == null)
             {
-                // Possibly a 404 error. Final catch-all if the website is finicky.
-                Console.Error.WriteLine(exception);
-                return null;
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(pageContent);
+
+                HtmlNode iconElement = doc.DocumentNode.SelectSingleNode("//link[@rel=\"icon\"]");
+                if (iconElement != null)
+                {
+                    faviconUrl = GenerateImageUrl(domain + "/", iconElement.Attributes["href"].Value);
+                    newFileName = DownloadImage(faviconUrl);
+                }
+                else
+                {
+                    return null;
+                }
             }
 
             return newFileName;
