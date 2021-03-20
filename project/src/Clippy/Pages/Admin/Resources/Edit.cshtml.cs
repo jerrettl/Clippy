71ba5186407a09ecc185ed40055e2b9cb4e8ec31
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-20 15:42:24 -0400

    Add notification when attempting to view non-existant resource

--- project/src/Clippy/Pages/Admin/Resources/Edit.cshtml.cs
+++ project/src/Clippy/Pages/Admin/Resources/Edit.cshtml.cs
@@ -24,7 +24,10 @@ namespace Clippy.Pages.Admin.Resources
         {
             var resource = await _context.GetResourceAsync(id);
             if (resource == null)
+            {
+                TempData["Message"] = $"Resource not found. Id = {id}.";
                 return RedirectToPage("./Index");
+            }
 
             Resource = new EditResourceModel
             {
