bc981bd0697ad80494b3faf777bfbe490caa329b
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-19 20:01:33 -0400

    Remove Old ClientApp React Folder

--- project/src/Clippy/ClientApp/src/components/BookmarkPage/__test__/BookmarkPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import BookmarkPage from './../BookmarkPage'
-
-it("renders without crashing", ()=>{
-  const div = document.createElement("div");
-  ReactDOM.render(<Router><BookmarkPage></BookmarkPage></Router>, div)
-})
