fa6fb52eb8c03b485ef23d1412dd262d88221ac1
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-21 17:11:58 -0400

    Add event ids for admin loggin events

---
+++ project/src/Clippy/Models/Admin/AdminLoggingEvents.cs
@@ -0,0 +1,9 @@
+namespace Clippy.Models.Admin
+{
+    public static class AdminLoggingEvents
+    {
+        public const int AddUserEvent = 1000;
+        public const int UpdateUserEvent = 1100;
+        public const int RemoveUserEvent = 1200;
+    }
+}
