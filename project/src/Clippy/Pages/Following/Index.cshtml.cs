5d10bd9c88e5e70f545f2f6a492659e559053a40
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-03-23 16:16:41 -0400

    Added following page skeleton

---
+++ project/src/Clippy/Pages/Following/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using Clippy.Data;
+using Clippy.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Clippy.Pages.Explore
+{
+    public class IndexModel : PageModel
+    {
+        private ClippyContext _context;
+
+        public IndexModel(ClippyContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Bookmark> Bookmarks { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Bookmarks = await _context.GetBookmarksAsync();
+        }
+    }
+}
