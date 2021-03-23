43f71a55cc844f3d64965d296b16c55f9eb86960
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-03-23 17:14:59 -0400

    Update Index.cshtml.cs
    Changed namespace to proper namespace

--- project/src/Clippy/Pages/Following/Index.cshtml.cs
+++ project/src/Clippy/Pages/Following/Index.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Clippy.Pages.Explore
+namespace Clippy.Pages.Following
 {
     public class IndexModel : PageModel
     {
