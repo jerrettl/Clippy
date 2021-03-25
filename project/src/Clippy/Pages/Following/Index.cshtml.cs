9b0405109f3713eb32e9a7167b605fe0a5448af7
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-03-25 18:18:24 -0400

    Unfollowing functionality on Following page implemented

--- project/src/Clippy/Pages/Following/Index.cshtml.cs
+++ project/src/Clippy/Pages/Following/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Clippy.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Clippy.Pages.Following
 {
@@ -18,6 +19,8 @@ namespace Clippy.Pages.Following
 
         public User ThisUser { get; set; }
 
+        public User UserBeingUnfollowed { get; set; }
+
         public async Task OnGetAsync()
         {
             string githubId = "";
@@ -27,6 +30,21 @@ namespace Clippy.Pages.Following
             }
 
             ThisUser = await _context.GetUserByGithubId(githubId);
+        }
+
+        public async Task<IActionResult> OnPostAsync(int thisuser, int viewinguser)
+        {
+            ThisUser = await _context.GetUserAsync(thisuser);
+            UserBeingUnfollowed = await _context.GetUserAsync(viewinguser);
+
+            if (ThisUser.Subscriptions.Contains(UserBeingUnfollowed))
+            {
+                ThisUser.Subscriptions.Remove(UserBeingUnfollowed);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Following/Index");
         }
     }
 }
