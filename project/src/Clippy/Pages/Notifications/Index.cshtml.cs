9aa38136aa1bd5e35a00f1c08103d6388b04e17b
BlackOutDevelops <77747282+BlackOutDevelops@users.noreply.github.com>
2021-03-25 20:05:01 -0400

    Skeleton for notifications page complete

---
+++ project/src/Clippy/Pages/Notifications/Index.cshtml.cs
@@ -0,0 +1,56 @@
+using Clippy.Data;
+using Clippy.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Clippy.Pages.Notifications
+{
+    public class IndexModel : PageModel
+    {
+        private ClippyContext _context;
+
+        public IndexModel(ClippyContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Bookmark> Bookmarks { get; set; }
+
+        public User ViewingUser { get; set; }
+
+        public User ThisUser { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            string githubId = "";
+            foreach (Claim claim in User.Claims)
+            {
+                if (claim.Type == ClaimTypes.NameIdentifier) githubId = claim.Value;
+            }
+
+            ThisUser = await _context.GetUserByGithubId(githubId);
+            if (id == 0)
+            {
+                ViewingUser = ThisUser;
+                id = ThisUser.Id;
+            }
+            else
+            {
+                ViewingUser = await _context.GetUserAsync(id);
+            }
+
+            if (ThisUser == null || ViewingUser == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            Bookmarks = await _context.GetBookmarksByUserIdAsync(id);
+
+            return Page();
+        }
+    }
+}
