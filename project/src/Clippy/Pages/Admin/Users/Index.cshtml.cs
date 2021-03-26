df09ffb849f9a781678a44b9af9bd2af72bb1bdb
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-26 17:09:35 -0400

    Add import and export of users in admin

--- project/src/Clippy/Pages/Admin/Users/Index.cshtml.cs
+++ project/src/Clippy/Pages/Admin/Users/Index.cshtml.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Clippy.Data;
 using Clippy.Entities;
+using Clippy.Models.Admin;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Clippy.Pages.Admin.Users
@@ -18,6 +22,41 @@ namespace Clippy.Pages.Admin.Users
 
         public async Task OnGetAsync() {
             Users = await _context.GetUsersAsync();
+        }
+
+        public async Task<IActionResult> OnPostExportAsync() {
+            var users = await _context.GetUsersExportAsync();
+
+            var exportedUsers = new List<ExportUserModel>();
+            foreach(var user in users)
+            {
+                var rb = new StringBuilder();
+                foreach(var role in user.Roles)
+                {
+                    rb.Append($"{role.Name},");
+                }
+
+                var sb = new StringBuilder();
+                foreach(var subscription in user.Subscriptions)
+                {
+                    sb.Append($"{subscription.Username},");
+                }
+
+                exportedUsers.Add(new ExportUserModel{
+                    Id = user.Id,
+                    Username = user.Username,
+                    Name = user.Name,
+                    AvatarUrl = user.AvatarUrl,
+                    Bio = user.Bio,
+                    Roles = rb.ToString().TrimEnd(','),
+                    Following = sb.ToString().TrimEnd(',')
+                });
+            }
+
+            return new JsonResult(exportedUsers, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
         }
     }
 }
