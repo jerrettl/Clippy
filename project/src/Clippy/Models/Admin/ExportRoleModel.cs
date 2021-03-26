ec26a03ad293f5f3a83838637c0765ff666bb971
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-26 18:27:35 -0400

    Add export roles for admin

---
+++ project/src/Clippy/Models/Admin/ExportRoleModel.cs
@@ -0,0 +1,8 @@
+namespace Clippy.Models.Admin
+{
+    public class ExportRoleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- project/src/Clippy/Pages/Admin/Roles/Index.cshtml.cs
+++ project/src/Clippy/Pages/Admin/Roles/Index.cshtml.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Clippy.Data;
 using Clippy.Entities;
+using Clippy.Models.Admin;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Clippy.Pages.Admin.Roles
@@ -18,6 +21,24 @@ namespace Clippy.Pages.Admin.Roles
 
         public async Task OnGetAsync() {
             Roles = await _context.GetRolesAsync();
+        }
+
+        public async Task<IActionResult> OnPostExportAsync() {
+            var roles = await _context.GetRolesAsync();
+
+            var exportedRoles = new List<ExportRoleModel>();
+            foreach(var role in roles)
+            {
+                exportedRoles.Add(new ExportRoleModel{
+                    Id = role.Id,
+                    Name = role.Name
+                });
+            }
+
+            return new JsonResult(exportedRoles, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
         }
     }
 }
