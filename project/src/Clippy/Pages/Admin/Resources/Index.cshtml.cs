8affe0c968d154f498eb9907f1d590152d7e2183
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-26 20:55:41 -0400

    Add export resources for admin

--- project/src/Clippy/Pages/Admin/Resources/Index.cshtml.cs
+++ project/src/Clippy/Pages/Admin/Resources/Index.cshtml.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Clippy.Data;
 using Clippy.Entities;
+using Clippy.Models.Admin;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Clippy.Pages.Admin.Resources
@@ -18,6 +21,25 @@ namespace Clippy.Pages.Admin.Resources
 
         public async Task OnGetAsync() {
             Resources = await _context.GetResourcesAsync();
+        }
+
+        public async Task<IActionResult> OnPostExportAsync() {
+            var resources = await _context.GetResourcesAsync();
+
+            var exportedResources = new List<ExportResourceModel>();
+            foreach(var resource in resources)
+            {
+                exportedResources.Add(new ExportResourceModel{
+                    Id = resource.Id,
+                    Location = resource.Location,
+                    Metadata = resource.Metadata
+                });
+            }
+
+            return new JsonResult(exportedResources, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
         }
     }
 }
