c5f09b07c66de1fe52e7b732485f09a938dff452
jordynhayden <51381594+jordynhayden@users.noreply.github.com>
2021-03-27 12:30:10 -0400

    Add import bookmark model

---
+++ project/src/Clippy/Models/Admin/ImportBookmarkModel.cs
@@ -0,0 +1,12 @@
+namespace Clippy.Models.Admin
+{
+    public class ImportBookmarkModel
+    {
+        public string Location { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Public { get; set; }
+        public string Tags { get; set; }
+        public string Username { get; set; }
+    }
+}
