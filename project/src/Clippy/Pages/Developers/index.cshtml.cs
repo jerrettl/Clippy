28e6007826ea9bf2f918f0de41627c2fb8acbebe
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-03-28 17:37:05 -0400

    Add developer page
    Co-Authored-By: Jerrett Longworth <jerrettl@users.noreply.github.com>
    Co-authored-by: Jaeivan Romero <jaerom@users.noreply.github.com>
    Co-Authored-By: Joshua Frazer <blackoutdevelops@users.noreply.github.com>

---
+++ project/src/Clippy/Pages/Developers/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clippy.Pages.Developers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
